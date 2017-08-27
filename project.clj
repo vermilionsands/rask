514806f6f6a9c4c8c96abfa37b24f76ca91ebc7e
vermilionsands <sands.vermilion@gmail.com>
2017-08-27 21:25:41 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject rask "0.1.0-SNAPSHOT"
+  :description "A Clojure DSL for Flink"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apache.flink/flink-streaming-java_2.10 "1.3.2"]
+                 [org.apache.flink/flink-java "1.3.2"]
+                 [org.apache.flink/flink-clients_2.10 "1.3.2"]])
+;; todo - move deps to provided
---
+++ src/rask/environment.clj
@@ -0,0 +1,115 @@
+(ns rask.environment
+  (:import [org.apache.flink.streaming.api.environment StreamExecutionEnvironment RemoteStreamEnvironment]
+           [org.apache.flink.streaming.api CheckpointingMode]
+           (java.util Collection)
+           (org.apache.flink.api.common.typeinfo TypeInformation)))
+
+;; environment
+
+(defn env
+  "Creates an execution environment that represents the context in which the program is currently executed."
+  []
+  (StreamExecutionEnvironment/getExecutionEnvironment))
+
+(defn local-env
+  "Creates a LocalStreamEnvironment."
+  ([]
+   (StreamExecutionEnvironment/createLocalEnvironment))
+  ([conf]
+   (StreamExecutionEnvironment/createLocalEnvironment
+     (StreamExecutionEnvironment/getDefaultLocalParallelism) conf)))
+
+(defn remote-env
+  "Creates a RemoteStreamEnvironment."
+  ([host port jars]
+   (StreamExecutionEnvironment/createRemoteEnvironment host port (into-array String jars)))
+  ([host port conf jars]
+   (RemoteStreamEnvironment. host port conf (into-array String jars))))
+
+(defn local-env-with-ui
+  "Creates a LocalStreamEnvironment for local program execution that also starts the web monitoring UI."
+  [conf]
+  (StreamExecutionEnvironment/createLocalEnvironmentWithWebUI conf))
+
+(defn execute
+  "Triggers the program execution. The environment will execute all parts of the program that have resulted
+  in a \"sink\" operation. Sink operations are for example printing results or forwarding them to a message queue.
+
+  The program execution will be logged and displayed with a generated default name or passed job-name."
+  ([env]
+   (.execute env))
+  ([env job-name]
+   (.execute env job-name)))
+
+;; configure
+
+(defn parallelism
+  "Gets/sets the parallelism for operations executed through this environment."
+  ([env]
+   (.getParallelism env))
+  ([env n]
+   (.setParallelism env n)))
+
+(defn max-parallelism
+  "Gets/sets the maximum degree of parallelism defined for the program."
+  ([env]
+   (.getMaxParallelism env))
+  ([env n]
+   (.setMaxParallelism env n)))
+
+(defn buffer-timeout
+  "Gets/sets the maximum time frequency (milliseconds) for the flushing of the output buffers."
+  ([env]
+   (.getBufferTimeout env))
+  ([env n]
+   (.setBufferTimeout env n)))
+
+(defn chaining? [env]
+  (.isChainingEnabled env))
+
+(defn disable-chaining [env]
+  (.disableOperatorChaining env))
+
+(defn checkpointing-mode
+  [mode]
+  (if (keyword? mode)
+    (mode {:exactly-once  CheckpointingMode/EXACTLY_ONCE
+           :at-least-once CheckpointingMode/AT_LEAST_ONCE})
+    mode))
+
+(defn checkpointing
+  ([env]
+   (.getCheckpointConfig env))
+  ([env interval]
+   (.enableCheckpointing env interval))
+  ([env interval mode]
+   (.enableCheckpointing env interval (checkpointing-mode mode))))
+
+;; datastreams
+
+(defn string-stream-from-socket
+  ([env host port]
+   (.socketTextStream env host port))
+  ([env host port del]
+   (.socketTextStream env host port del))
+  ([env host port del max-retry]
+   (.socketTextStream env host port del max-retry)))
+
+(defn string-stream-from-file
+  ([env path]
+   (.readTextFile env path))
+  ([env path charset]
+   (.readTextFile env path charset)))
+
+(defn stream-from-collection
+  ([env xs]
+   (.fromCollection env ^Collection xs))
+  ([env xs type-info]
+   (let [type-info (if (instance? TypeInformation type-info)
+                     type-info
+                     (TypeInformation/of ^Class type-info))]
+     (.fromCollection env ^Collection xs ^TypeInformation type-info))))
+
+(defn object-stream-from-collection
+  [env xs]
+  (.fromCollection env xs (TypeInformation/of ^Class Object)))
