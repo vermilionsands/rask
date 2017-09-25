f285aab3b62a611f672bf0370686fec8e24e25cf
vermilionsands <sands.vermilion@gmail.com>
2017-09-25 22:22:03 +0200

    Adding map, filter, removing requirement for argument to be a coll in by-key and updating docs.

--- src/clojure/rask/datastream.clj
+++ src/clojure/rask/datastream.clj
@@ -1,7 +1,7 @@
 (ns rask.datastream
-  (:refer-clojure :exclude [print reduce])
-  (:require [rask.data :as data])
-  (:import [org.apache.flink.api.common.functions FlatMapFunction ReduceFunction]
+  (:refer-clojure :exclude [filter map print reduce])
+  (:require [rask.util :as util])
+  (:import [org.apache.flink.api.common.functions FlatMapFunction ReduceFunction MapFunction FilterFunction]
            [org.apache.flink.util Collector]
            [org.apache.flink.streaming.api.datastream DataStream KeyedStream SingleOutputStreamOperator
                                                       DataStreamSink WindowedStream]
@@ -9,8 +9,20 @@
            [org.apache.flink.api.java.functions KeySelector]
            [rask.api RequiringFunction]))
 
-(defn flat-map
-  "Accepts a function f that should take one argument and return a sequence of results
+(defn ^DataStream map
+  "Takes one element and produces one element.
+
+  Accepts a one arg function"
+  [f ^DataStream stream]
+  (let [p (proxy [RequiringFunction MapFunction] [f]
+            (map [x]
+              (f x)))]
+    (.map stream p)))
+
+(defn ^DataStream flat-map
+  "Takes one element and produces zero, one, or more elements.
+
+  Accepts a function f that should take one argument and return a sequence of results
   that would be added to collector."
   [f ^DataStream stream]
   (let [p (proxy [RequiringFunction FlatMapFunction] [f]
@@ -19,34 +31,64 @@
                 (.collect acc y))))]
     (.flatMap stream p)))
 
-(defn reduce
-  "Accepts a function f that takes two arguments."
+(defn ^DataStream filter
+  "Evaluates a predicate for each element and retains those for which the predicate returns true.
+
+  Accepts a one arg function that would be coerced to boolean."
+  [f ^DataStream stream]
+  (let [p (proxy [RequiringFunction FilterFunction] [f]
+            (filter [x]
+              (boolean (f x))))]
+    (.filter stream p)))
+
+(defn ^DataStream reduce
+  "For KeyedStream:
+  A \"rolling\" reduce on a keyed data stream. Combines the current element with the last reduced value and
+  emits the new value.
+
+  For WindowedStream:
+  Applies a functional reduce function to the window and returns the reduced value.
+
+  Accepts a function f that takes two arguments."
   [f ^DataStream stream]
   (let [p (proxy [RequiringFunction ReduceFunction] [f]
             (reduce [x y]
               (f x y)))]
-    ;; a bit ugly
     (cond
       (instance? KeyedStream stream) (.reduce ^KeyedStream stream p)
       (instance? WindowedStream stream) (.reduce ^WindowedStream stream p)
-      :else (.reduce stream p))))
+      :else (throw
+              (IllegalArgumentException.
+                (format "Unsupported stream type: %s" (class stream)))))))
 
-(defn by-key
-  "Partitions the operator state of a stream by the given key positions.
+(defn ^KeyedStream by-key
+  "Logically partitions a stream into disjoint partitions, each partition containing elements of the same key.
 
    Accepts fields which be can be a sequence of:
    * indexes
    * names of a public fields
    * getter methods with parentheses of the stream underlying type
-   or a KeySelector and a stream"
-  [fields-or-key-selector ^DataStream stream]
-  (if (or (seq? fields-or-key-selector)
-          (vector? fields-or-key-selector))
-    (let [[field :as fields] fields-or-key-selector]
-      (cond
-        (number? field) (.keyBy stream ^ints (int-array fields))
-        (string? field) (.keyBy stream ^"[Ljava.lang.String;" (into-array String fields))))
-    (.keyBy stream ^KeySelector fields-or-key-selector)))
+   or a KeySelector and a stream."
+  [key ^DataStream stream]
+  (if (instance? KeySelector key)
+    (.keyBy stream ^KeySelector key)
+    (cond
+      (sequential? key)
+      (let [[first-key] key]
+        (cond
+          (number? first-key) (.keyBy stream ^ints (int-array key))
+          (string? first-key) (.keyBy stream ^"[Ljava.lang.String;" (into-array String key))))
+
+      (number? key)
+      (.keyBy stream ^ints (int-array [key]))
+
+      (string? key)
+      (.keyBy stream ^"[Ljava.lang.String;" (into-array String [key]))
+
+      :else
+      (throw
+        (IllegalArgumentException.
+          (format "Unsupported key %s: " (class key)))))))
 
 (defn sum
   "Applies an aggregation that gives a rolling sum of the data stream at the given position grouped by the given key.
@@ -65,9 +107,9 @@
   size and slide should be either number of milliseconds, or instances of
   org.apache.flink.streaming.api.windowing.time.Time"
   ([size ^KeyedStream stream]
-   (.timeWindow stream (data/time size)))
+   (.timeWindow stream (util/time size)))
   ([size slide ^KeyedStream stream]
-   (.timeWindow stream (data/time size) (data/time slide))))
+   (.timeWindow stream (util/time size) (util/time slide))))
 
 (defn returns
   "Adds a type information hint about the return type of this operator.
