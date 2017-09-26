3096fd59a91121524db0e8a65d506b4854be896d
vermilionsands <sands.vermilion@gmail.com>
2017-09-27 01:07:27 +0200

    Fixing namespace solving in raskfn.

--- src/clojure/rask/api.clj
+++ src/clojure/rask/api.clj
@@ -3,9 +3,10 @@
 (defmacro raskfn
   "Like fn, but stores it's namespace symbol in metadata under :rask.api/namespace."
   [& body]
-  `(with-meta
-     (fn ~@body)
-     {::namespace (ns-name *ns*)}))
+  (let [namespace (ns-name *ns*)]
+    `(with-meta
+       (fn ~@body)
+       {::namespace '~namespace})))
 
 (defmacro defraskfn
   "Like defn, but stores it's namespace symbol in metadata under :rask.api/namespace."
