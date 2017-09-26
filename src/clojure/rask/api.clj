959206a810299094fed27a8056d8acf78ad2b1e7
vermilionsands <sands.vermilion@gmail.com>
2017-09-27 00:35:14 +0200

    Adding raskfn macro.

--- src/clojure/rask/api.clj
+++ src/clojure/rask/api.clj
@@ -1,9 +1,14 @@
 (ns rask.api)
 
+(defmacro raskfn
+  "Like fn, but stores it's namespace symbol in metadata under :rask.api/namespace."
+  [& body]
+  `(with-meta
+     (fn ~@body)
+     {::namespace (ns-name *ns*)}))
+
 (defmacro defraskfn
-  "Like defn, but stores its namespace symbol in metadata under :rask.api/namespace."
+  "Like defn, but stores it's namespace symbol in metadata under :rask.api/namespace."
   [name & body]
   `(def ~name
-     (with-meta
-       (fn ~@body)
-       {::namespace (ns-name *ns*)})))
+     (raskfn ~@body)))
