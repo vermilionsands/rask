c16a203f67bb4b27ed7003a8445840e5535edcc4
vermilionsands <sands.vermilion@gmail.com>
2017-09-30 13:43:40 +0200

    Suppressing deprecation warning on FoldFn.

--- src/java/rask/api/FoldFn.java
+++ src/java/rask/api/FoldFn.java
@@ -1,9 +1,9 @@
 package rask.api;
 
 import clojure.lang.IFn;
-import org.apache.flink.api.common.functions.FoldFunction;
 
-public class FoldFn extends RequiringFunction implements FoldFunction {
+@SuppressWarnings("deprecation")
+public class FoldFn extends RequiringFunction implements org.apache.flink.api.common.functions.FoldFunction {
 
   public FoldFn(IFn fn) {
     super(fn);
