04fc2d868c8e44486a8978c54598cdd162da3843
vermilionsands <sands.vermilion@gmail.com>
2018-07-31 21:49:09 +0200

    Sample text data.

--- examples/src/clojure/rask/examples/samples.clj
+++ examples/src/clojure/rask/examples/samples.clj
@@ -1,3 +1,24 @@
 (ns rask.examples.samples)
 
-(def words [])
+(def words
+  "Sample text dataset"
+  ["I have always found that Angels have the vanity to speak of themselves as the only wise;"
+   "this they do with a confident insolence sprouting from systematic reasoning:"
+   "Thus Swedenborg boasts that what he writes is new;"
+   "tho' it is only the Contents or Index of already publish'd books."
+   "A man carried a monkey about for a shew,"
+   "& because he was a little wiser than the monkey,"
+   "grew vain, and conciev'd himself as much wiser than seven men."
+   "It is so with Swedenborg;"
+   "he shews the folly of churches & exposes hypocrites,"
+   "till he imagines that all are religious,"
+   "& himself the single one on earth that ever broke a net."
+   "Now hear a plain fact: Swedenborg has not written one new truth:"
+   "Now hear another: he has written all the old falshoods."
+   "And now hear the reason. He conversed with Angels who are all religious,"
+   "& conversed not with Devils who all hate religion, for he was incapable thro' his conceited notions."
+   "Thus Swedenborgs writings are a recapitulation of all superficial, opinions, and an analysis of the more sublime, but no further."
+   "Have now another plain fact:"
+   "Any man of mechanical talents may from the writings of Paracelsus or Jacob Behmen,"
+   "produce ten thousand volumes of equal value with Swedenborgs, and from those of Dante or Shakespear, an infinite number."
+   "But when he has done this, let him not say that he knows better than his master, for he only holds a candle in sunshine."])
